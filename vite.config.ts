8b2d44b37b21559baaf79316ca1afefaebd5aa91
Roman <r89534217610@gmail.com>
2022-09-23 11:59:42 +0400

    env setup + lib building

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,38 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import { resolve } from "path";
+
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
   base: "/vue-smoothie/",
+
+  publicDir: mode === "lib" ? false : "public",
+
   build: {
+    outDir: mode === "lib" ? "dist_lib" : "dist",
+
     rollupOptions: {
       output: {
         entryFileNames: `[name].js`,
         chunkFileNames: `[name].js`,
         assetFileNames: `[name].[ext]`,
       },
+
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: mode === "lib" ? ["vue"] : undefined,
     },
+
+    lib:
+      mode === "lib"
+        ? {
+            entry: resolve(__dirname, "index.js"),
+            name: "Smoothie Components Library",
+            formats: ["es"],
+            // filename: "smoothie.js",
+          }
+        : undefined,
   },
-});
+}));
