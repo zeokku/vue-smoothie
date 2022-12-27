bf692d4b9a137ba09c9d76bb9dac7b51378e975f
Lutymane <r89534217610@gmail.com>
2022-12-27 17:15:53 +0300

    fix types

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,9 @@ import type { DefineComponent } from "vue";
 
 type SmoothieBase = DefineComponent<{ weight?: number }> & { el: HTMLElement };
 
-declare const Smoothie: SmoothieBase & { y: number };
-declare const OmniSmoothie: SmoothieBase & { x: number; y: number };
-
-export { Smoothie, OmniSmoothie };
+declare module "vue-smoothie" {
+  declare const Smoothie: SmoothieBase & { y: number };
+  declare const OmniSmoothie: SmoothieBase & { x: number; y: number };
+  
+  export { Smoothie, OmniSmoothie };
+}
